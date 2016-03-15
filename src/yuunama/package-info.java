903f2a18de64770080f5e18bbeec407d8110635c
TOSUKUi <am.businessmail.exp@gmail.com>
2016-03-16 04:36:11 +0900

    make document

--- src/yuunama/package-info.java
+++ src/yuunama/package-info.java
@@ -1,8 +1,8 @@
 /**
  * Created by TOSUKUi on 2016/03/15.
- * 勇者のくせになまいきだ。ソーストップパッケージ
- * systems:ゲームの動作部分を管理するパッケージ
- * swinggui:swingによるGUI描画用パッケージ
- * images:描画用の画像群
+ * 勇者のくせになまいきだ。ソーストップパッケージ<br>
+ * systems:ゲームの動作部分を管理するパッケージ<br>
+ * swinggui:swingによるGUI描画用パッケージ<br>
+ * images:描画用の画像群<br>
  */
 package yuunama;
--- src/yuunama/systems/package-info.java
+++ src/yuunama/systems/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Created by TOSUKUi on 2016/03/16.
- * ゲームの動作部分を管理するパッケージ
- * entity:マモノや勇者などの動的なオブジェクト
- * structure:ダンジョンブロックなどのオブジェクト
+ * ゲームの動作部分を管理するパッケージ<br>
+ * entity:マモノや勇者などの動的なオブジェクト<br>
+ * structure:ダンジョンブロックなどのオブジェクト<br>
  */
 package yuunama.systems;
